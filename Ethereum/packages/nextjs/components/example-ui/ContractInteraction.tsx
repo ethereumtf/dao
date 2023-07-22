589bbe96bc7657391cd307a19f4c57e2463c7359
Benjamin <myethereumtf@gmail.com>
2023-07-22 11:25:00 +0200

    Update ContractInteraction.tsx

--- Ethereum/packages/nextjs/components/example-ui/ContractInteraction.tsx
+++ Ethereum/packages/nextjs/components/example-ui/ContractInteraction.tsx
@@ -21,24 +21,16 @@ export const ContractInteraction = () => {
 
   return (
     <div className="flex bg-base-300 relative pb-10">
-      <DiamondIcon className="absolute top-24" />
-      <CopyIcon className="absolute bottom-0 left-36" />
-      <HareIcon className="absolute right-0 bottom-24" />
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
         <div className={`mt-10 flex gap-2 ${visible ? "" : "invisible"} max-w-2xl`}>
           <div className="flex gap-5 bg-base-200 bg-opacity-80 z-0 p-7 rounded-2xl shadow-lg">
-            <span className="text-3xl">👋🏻</span>
+            <span className="text-3xl">📌</span>
             <div>
               <div>
-                In this page you can see how some of our <strong>hooks & components</strong> work, and how you can bring
-                them to life with your own design! Have fun and try it out!
+                Select a token to grow during the investment period and enter <strong>the amount of Ethereum</strong> to invest!
               </div>
               <div className="mt-2">
-                Check out{" "}
-                <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem]">
-                  packages / nextjs/pages / example-ui.tsx
-                </code>{" "}
-                and its underlying components.
+                Click the <strong>"vote"</strong> button to see the voting results and current status.
               </div>
             </div>
           </div>
@@ -51,7 +43,7 @@ export const ContractInteraction = () => {
         </div>
 
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Set a Greeting_</span>
+          <span className="text-3xl sm:text-4xl text-black">DAO vote!</span>
 
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <input
@@ -60,6 +52,7 @@ export const ContractInteraction = () => {
               className="input font-bai-jamjuree w-full px-5 bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white uppercase"
               onChange={e => setNewGreeting(e.target.value)}
             />
+
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
                 <button
@@ -76,6 +69,37 @@ export const ContractInteraction = () => {
                 </button>
               </div>
             </div>
+          </div>
+
+
+          <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
+            <input type="number" min="0" placeholder="Amount of token" className="input font-bai-jamjuree w-full px-5 bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white uppercase" />
+          </div>
+
+
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-5">
+            <label className="font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest flex-row-reverse">
+              <input type="checkbox" name="option1" value="option1" className="" />
+              Ethereum
+            </label>
+            <label className="font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest flex-row-reverse">
+              <input type="checkbox" name="option2" value="option2" className="" />
+              Tether USD
+            </label>
+            <label className="font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest flex-row-reverse">
+              <input type="checkbox" name="option3" value="option3" className="" />
+              USD Coin
+            </label>
+            <label className="font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest flex-row-reverse">
+              <input type="checkbox" name="option4" value="option4" className="" />
+              Binance USD
+            </label>
+          </div>
+
+          <div>
+            <button className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest">
+              Vote
+            </button>
           </div>
 
           <div className="mt-4 flex gap-2 items-start">
